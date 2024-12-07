c692d355300255c958022c7fb67d5a2547bfd388
dzajac <dzajac95@gmail.com>
2024-12-07 16:02:37 -0500

    day 2 part 2, done!

---
+++ day2/part2.ml
@@ -0,0 +1,40 @@
+open Common
+
+let check_report report =
+    let l = List.filteri (fun i _ -> i < (List.length report) - 1) report in
+    let r = List.tl report in
+    let diffs = List.map2 (fun a b -> b - a) l r in
+    (List.for_all (fun x -> x < 0) diffs || List.for_all (fun x -> x > 0) diffs) &&
+    List.for_all (fun x -> 1 <= x && x <= 3) (List.map (fun x -> Int.abs x) diffs)
+
+let rec check_report_filt report idx =
+    if idx >= List.length report then
+        false
+    else if check_report (List.filteri (fun i _ -> i != idx) report) then
+        true
+    else
+        check_report_filt report (idx+1)
+
+let () =
+    let file = open_in "./day2/input.txt" in
+    let lines = Util.get_lines file in
+    let parse_line line =
+        List.map (fun s -> int_of_string s) (String.split_on_char ' ' line) in
+    let reports = List.map parse_line lines in
+    let print_report report result =
+        let rec loop rem =
+            match rem with
+            | [] -> ""
+            | [x] -> string_of_int x
+            | x::rest -> (string_of_int x) ^ " " ^ (loop rest) in
+        print_string "[";
+        print_string (loop report);
+        print_string "] -> ";
+        if result then
+            print_endline "GOOD"
+        else
+            print_endline "BAD" in
+    let report_results = List.map (fun report -> check_report report || check_report_filt report 0) reports in
+    List.iter2 print_report reports report_results;
+    let total_safe = List.fold_left (fun acc _ -> acc + 1) 0 (List.filter (fun x -> x) report_results) in
+    print_endline ("Total safe reports: " ^ (string_of_int total_safe))
